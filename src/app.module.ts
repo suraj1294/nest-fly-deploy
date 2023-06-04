fed9126b6ca5463ebfd44c5cb696a63184bd8420
ipalace <ipalace@ipalaces-MacBook-Pro.local>
2023-06-04 21:46:41 +0530

    setup fly and docker for nest js

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ApiModule } from './api/api.module';
+import { User } from './api/user/user.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['development.env', '.env'],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: ['dist/**/*.entity.{ts,js}', User],
+      migrations: ['dist/migrations/*.{ts,js}'],
+      migrationsTableName: 'typeorm_migrations',
+      logger: 'file',
+      synchronize: true,
+    }),
+    ApiModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
